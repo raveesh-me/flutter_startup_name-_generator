50c8acabb914d6fb56fd2915e970ae96e607e648
Raveesh Agarwal <hi@raveeshagarwal.in>
2020-04-23 14:37:54 +0530

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutterstartupnamegenerator/src/favorites_screen.dart';
+import 'package:flutterstartupnamegenerator/src/home_screen.dart';
 
-main(){
+main() {
   runApp(StartupNameGeneratorApp());
 }
 
 class StartupNameGeneratorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      routes: {
+        HomeScreen.routeName: (_) => HomeScreen(),
+        FavoritesScreen.routeName: (_) => FavoritesScreen(
+              favoriteNames: ModalRoute.of(_).settings.arguments,
+            ),
+      },
+      initialRoute: '/',
+    );
   }
 }
