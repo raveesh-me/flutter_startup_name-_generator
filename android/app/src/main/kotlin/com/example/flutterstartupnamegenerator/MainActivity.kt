0ce02f033cd4e43a59d6b2791601f5b455d51c51
Raveesh Agarwal <hi@raveeshagarwal.in>
2020-04-23 13:31:45 +0530

    initial repo

---
+++ android/app/src/main/kotlin/com/example/flutterstartupnamegenerator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterstartupnamegenerator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(StartupNameGeneratorApp());
+}
+
+class StartupNameGeneratorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
