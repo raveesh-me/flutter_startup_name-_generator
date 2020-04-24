fb142ed2e6e781c0e2d52afc9368988383468516
Raveesh Agarwal <hi@raveeshagarwal.in>
2020-04-24 12:54:13 +0530

    implements shared preferences

--- lib/src/home_screen.dart
+++ lib/src/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:flutterstartupnamegenerator/src/favorites_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
-
   static final String routeName = '/';
+
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
@@ -12,12 +13,39 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List<String> startupNames = [];
   Set<String> favoriteNames = Set<String>();
+  bool isFetchingFromSP = true;
+  final String savekey = 'favorites';
+
+  SharedPreferences preferences;
 
   @override
   void initState() {
     super.initState();
     startupNames
         .addAll(List.generate(30, (index) => WordPair.random().asPascalCase));
+    fetchSP();
+  }
+
+  fetchSP() async {
+    setState(() {
+      isFetchingFromSP = true;
+    });
+    preferences = await SharedPreferences.getInstance();
+    List<String> savedFavorites = preferences.getStringList(savekey);
+    if (savedFavorites == null) {
+      preferences.setStringList(savekey, <String>[]);
+      savedFavorites = <String>[];
+    }
+    favoriteNames.addAll(savedFavorites);
+    startupNames = [...savedFavorites, ...startupNames];
+    setState(() {
+      isFetchingFromSP = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   @override
@@ -37,33 +65,44 @@ class _HomeScreenState extends State<HomeScreen> {
           )
         ],
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          if (index >= startupNames.length)
-            startupNames.addAll(
-                List.generate(20, (index) => WordPair.random().asPascalCase));
-          final String startupName = startupNames[index];
-          return ListTile(
-            title: Text(startupName),
-            trailing: IconButton(
-              icon: favoriteNames.contains(startupName)
-                  ? Icon(Icons.favorite)
-                  : Icon(Icons.favorite_border),
-              color: favoriteNames.contains(startupName)
-                  ? Colors.red
-                  : Colors.black54,
-              onPressed: () {
-                // what happens when Icon is pressed
-                print('$startupName pressed');
-                setState(() {
-                  favoriteNames.contains(startupName)
-                      ? favoriteNames.remove(startupName)
-                      : favoriteNames.add(startupName);
-                });
+      body: Column(
+        children: <Widget>[
+          if (isFetchingFromSP) LinearProgressIndicator(),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                if (index >= startupNames.length)
+                  startupNames.addAll(List.generate(
+                      20, (index) => WordPair.random().asPascalCase));
+                final String startupName = startupNames[index];
+                return ListTile(
+                  title: Text(startupName),
+                  trailing: IconButton(
+                    icon: favoriteNames.contains(startupName)
+                        ? Icon(Icons.favorite)
+                        : Icon(Icons.favorite_border),
+                    color: favoriteNames.contains(startupName)
+                        ? Colors.red
+                        : Colors.black54,
+                    onPressed: () {
+                      // what happens when Icon is pressed
+                      print('$startupName pressed');
+                      setState(() {
+                        favoriteNames.contains(startupName)
+                            ? favoriteNames.remove(startupName)
+                            : favoriteNames.add(startupName);
+                        final Set favorites =
+                            preferences.getStringList(savekey)?.toSet();
+                        favorites.add(startupName);
+                        preferences.setStringList(savekey, favorites.toList());
+                      });
+                    },
+                  ),
+                );
               },
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
